112da81efd1f5451c34d156bdb3b34fac6343d46
FeiweiPeng <fepeng@ucsd.edu>
2023-04-20 17:25:21 -0700

    fix bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -27,4 +27,24 @@ public class ArrayTests {
     int[] intlist2 = {1, 2, 3};
     assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(intlist2));
   }
+  
+  @Test
+  public void testintAverage(){
+    double[] intlist3 = {0.0, 2.0, 4, 5, 6, 8};
+    double result = 25.0/5;
+    assertEquals(result, ArrayExamples.averageWithoutLowest(intlist3),0.0001);
+  }
+
+  @Test
+  public void testintAverageEmpty(){
+    double[] emplist = {};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(emplist), 0.0001);
+  }
+
+  @Test
+  public void testintAveragemulti(){
+    double[] intlist4 = {3, 3, 4, 5, 3};
+    double result1 = 9.0/2;
+    assertEquals(result1, ArrayExamples.averageWithoutLowest(intlist4), 0.0001);
+  }
 }
